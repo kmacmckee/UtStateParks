ca8fb83fa4f28cf889aa67c5994cad85db81641b
Kobe McKee <kobe.mckee@gmail.com>
2019-08-22 18:22:16 -0600

    Added git ignore

---
+++ UtStateParks/Networking/Config.swift
@@ -0,0 +1,18 @@
+//
+//  APIInfo.swift
+//  UtStateParks
+//
+//  Created by Kobe McKee on 8/22/19.
+//  Copyright © 2019 Kobe McKee. All rights reserved.
+//
+
+import Foundation
+class APIInfo {
+    
+    private let parksAPIKey = "oEB2w0Kdr1T85hF5FIeNIYgXdl11M023Q7pjkXGS"
+    
+    private let trailsAPIKey = "200554629-1d6c855aff169c41e8ea471dd91a0c9f"
+    
+    private let forecastAPIKet = "200554629-1d6c855aff169c41e8ea471dd91a0c9f"
+    
+}
